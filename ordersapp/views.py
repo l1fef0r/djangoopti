c98cadd6d47aed8018172441962730cb32dc374e
l1fef0r <alexunder.stepanoff@gmail.cm>
2021-08-07 14:47:40 +0500

    добавил обработку заказа. Просмотр списка заказа, создание заказа. для заказа создал модель, создание форм, передачу по урл и контроль рендеринга с проверкой валидности

---
+++ ordersapp/views.py
@@ -0,0 +1,52 @@
+from django.db import transaction
+from django.forms import inlineformset_factory
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+
+from ordersapp.forms import OrderItemForm
+from ordersapp.models import Order, OrderItem
+
+
+class OrderList(ListView):
+    model = Order
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+
+class OrderItemCreate(CreateView):
+    model = Order
+
+    fields = []
+
+    success_url = reverse_lazy('ordersapp:orders_list')
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        OrderFormset = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.method == 'POST':
+
+            formset =OrderFormset(self.request.POST)
+
+        else:
+            formset = OrderFormset()
+
+        data['orderitems'] = formset
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+
+            self.object = form.save()
+
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        return super().form_valid(form)
