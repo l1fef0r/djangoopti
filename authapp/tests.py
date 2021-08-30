83c91a19a4287cd8ea6f01834a21de97b8ae3dfd
Alexaner <alexunder.stepanoff@gmail.com>
2021-08-31 02:06:02 +0500

    добавил тест на логин, проерка урлов и проверку модели продуктов

--- authapp/tests.py
+++ authapp/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from django.test.client import Client
+from authapp.models import ShopUser
 
-# Create your tests here.
+
+class TestUserManagement(TestCase):
+    username = 'django2'
+    user_password = 'geekbrains'
+
+    def setUp(self):
+        self.client = Client()
+
+        self.user = ShopUser.objects.create_user(
+            username=self.username,
+            email='django2@dj.local',
+            password=self.user_password,
+        )
+
+    def test_login_user(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.assertNotContains(response, 'Пользователь', status_code=200)
+
+        user_data = {
+            'username': self.username,
+            'password': self.user_password
+        }
+
+        response = self.client.post('/auth/login/', data=user_data)
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.assertNotContains(response, 'Пользователь', status_code=200)
+
+
+class TestUserManagement(TestCase):
+    username = 'django2'
+    user_password = 'geekbrains'
+
+    def setUp(self):
+        self.client = Client()
+
+        self.user = ShopUser.objects.create_user(
+            username=self.username,
+            email='django2@dj.local',
+            password=self.user_password,
+        )
+
+    def test_basket_login_redirect(self):
+           # без логина должен переадресовать
+           response = self.client.get('/basket/')
+           self.assertEqual(response.url, '/?next=/basket/')
+           self.assertEqual(response.status_code, 302)
+
+           # с логином все должно быть хорошо
+           self.client.login(username=self.username, password=self.user_password)
+
+           response = self.client.get('/basket/')
+           self.assertEqual(response.status_code, 200)
+           self.assertEqual(list(response.context['basket']), [])
+           self.assertEqual(response.request['PATH_INFO'], '/basket/')
+           self.assertIn('Ваша корзина, Пользователь', response.content.decode())
